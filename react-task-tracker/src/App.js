adc9bbbdce0bfcdfffd905ccb9d52e6a54d79761
Patchara Kleebbua <patchara.pck@gmail.com>
2021-08-02 13:29:02 +0700

    UI complete

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const[showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState(
     [
         {
@@ -27,6 +29,14 @@ function App() {
     ]
 )
 
+// Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000)+1
+    console.log(id)
+    const newTask = {id,...task}
+    setTasks([...tasks,newTask])
+  }
+
 // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,8 +49,9 @@ function App() {
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
